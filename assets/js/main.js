7af88b73074f6143a4ff4f9ba8e1d67032cf3a64
Rakib013 <mdrakib32661@gmail.com>
2021-12-15 11:09:38 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav__menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav__close")
+    
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener("click", ()=>{
+        navMenu.classList.add("show-menu");
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener("click", ()=>{
+        navMenu.classList.remove("show-menu");
+    })
+}
+
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navmenu = document.getElementById("nav__menu");
+    navmenu.classList.remove("show-menu");
+}
+navLink.forEach(e => e.addEventListener('click', linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleAction(){
+    let itemClass = this.parentNode.className;
+    for(i = 0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skill__open';
+    }
+}
+
+skillsHeader.forEach(e => {
+    e.addEventListener('click', toggleAction)
+    console.log(e);
+    }
+);
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualication__active')
+        })
+        target.classList.add('qualication__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualication__active')
+        })
+        tab.classList.add('qualication__active');
+    })
+})
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
